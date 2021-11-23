195c03bfc35a6706c276276220c849d30935fa74
Kristian Toustrup Langborg-Hansen <ktlh@smartlearning.dk>
2021-11-23 16:54:34 +0100

    Tilføjet mere dokumentation

--- Co2Besparelse/Koordinatberegner.cs
+++ Co2Besparelse/Koordinatberegner.cs
@@ -10,11 +10,21 @@ namespace Co2Besparelse
     {
         private string key;
 
+        /// <summary>
+        /// Initialiserer objektet
+        /// </summary>
+        /// <param name="key">Den nøgle der skal bruges til at kalde servicen</param>
         public Koordinatberegner(string key)
         {
             this.key = key;
         }
 
+        /// <summary>
+        /// 
+        /// Konverterer en adresse til et sæt GPS-koordinater
+        /// </summary>
+        /// <param name="adresse">Den adresse, der skal konverteres (fx Nansgaden 19 København)</param>
+        /// <returns>Adressens koordinatsæt</returns>
         public async Task<Koordinatset> HentKoordinaterForAdresse(string adresse)
         {
             return await AfstandService.AdresseTilKoordinater(key, adresse);            
--- Co2Besparelse/Koordinatset.cs
+++ Co2Besparelse/Koordinatset.cs
@@ -1,6 +1,9 @@
 ﻿
 namespace Co2Besparelse
 {
+    /// <summary>
+    /// Repræsenterer et sæt GPS-koordinater
+    /// </summary>
     internal class Koordinatset
     {
         public double Longitude { get; set; }
